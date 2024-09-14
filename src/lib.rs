d275a8d3a3496dbab6b768f22817dbdfd999fbdd
inadati <dr.inadati@gmail.com>
2024-09-14 12:35:49 +0900

    GoogleOidcClientにnewメソッドを追加 (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,10 @@ use act::google::GoogleOidcClientAct;
 
 pub struct GoogleOidcClient;
 
+impl GoogleOidcClient {
+    pub fn new() -> Self {
+        Self{}
+    }
+}
+
 impl GoogleOidcClientAct for GoogleOidcClient {}
